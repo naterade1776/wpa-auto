238f5bfd9708e8e9416d8fa39805845d0029360b
Nate K <97563728+naterade1776@users.noreply.github.com>
2023-11-30 14:19:22 -0700

    Delete start-wpa.sh
    Deprecated, as wpa-auto can now initialize and restart wpa_supplicant when needed.

--- start-wpa.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ "$EUID" -ne 0 ]
-  then echo "Please change to root user. Not sudo/doas."
-else (wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant-wlan0.conf)
-fi
-exit
